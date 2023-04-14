a00f32f3e4ca086393f95b711aca1bb90df839bb
wenzdaywww <289209496@qq.com>
2023-04-14 23:02:23 +0800

    DateUtils增加方法

--- src/main/java/com/www/common/utils/DateUtils.java
+++ src/main/java/com/www/common/utils/DateUtils.java
@@ -16,6 +16,138 @@ import java.util.Date;
  */
 public class DateUtils {
     /**
+     * <p>@Description 获取日期所在月份的第一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:18 </p>
+     * @param date 日期
+     * @return 日期所在月份的第一天
+     */
+    public static Date monthFirstDay(Date date){
+        if(date == null){
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int firstDay = c.getActualMinimum(Calendar.DAY_OF_MONTH);//获取某月最小天数
+        c.set(Calendar.DAY_OF_MONTH,firstDay);//设置日历中月份的最小天数
+        return c.getTime();
+    }
+    /**
+     * <p>@Description 获取日期所在月份的第一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:19 </p>
+     * @param date 日期
+     * @param formatEnum 日期格式
+     * @return 日期所在月份的第一天
+     */
+    public static String monthFirstDay(Date date,DateFormatEnum formatEnum){
+        Date firstDate = monthFirstDay(date);
+        return format(firstDate,formatEnum);
+    }
+    /**
+     * <p>@Description 获取日期所在月份的最后一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:18 </p>
+     * @param date 日期
+     * @return 日期所在月份的第一天
+     */
+    public static Date monthLastDay(Date date){
+        if(date == null){
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int lastDay = c.getActualMaximum(Calendar.DAY_OF_MONTH);//获取某月最大天数
+        c.set(Calendar.DAY_OF_MONTH,lastDay);//设置日历中月份的最大天数
+        return c.getTime();
+    }
+    /**
+     * <p>@Description 获取日期所在月份的最后一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:19 </p>
+     * @param date 日期
+     * @param formatEnum 日期格式
+     * @return 日期所在月份的第一天
+     */
+    public static String monthLastDay(Date date,DateFormatEnum formatEnum){
+        Date firstDate = monthLastDay(date);
+        return format(firstDate,formatEnum);
+    }
+    /**
+     * <p>@Description 获取日期所在年份的第一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:18 </p>
+     * @param date 日期
+     * @return 日期所在年份的第一天
+     */
+    public static Date yearFirstDay(Date date){
+        if(date == null){
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int lastDay = c.getActualMinimum(Calendar.DAY_OF_YEAR);//获取某年最小天数
+        c.set(Calendar.DAY_OF_YEAR,lastDay);//设置日历中年份的最小天数
+        return c.getTime();
+    }
+    /**
+     * <p>@Description 获取日期所在年份的第一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:19 </p>
+     * @param date 日期
+     * @param formatEnum 日期格式
+     * @return 日期所在年份的第一天
+     */
+    public static String yearFirstDay(Date date,DateFormatEnum formatEnum){
+        Date firstDate = yearFirstDay(date);
+        return format(firstDate,formatEnum);
+    }
+    /**
+     * <p>@Description 获取日期所在年份的最后一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:18 </p>
+     * @param date 日期
+     * @return 日期所在年份的最后一天
+     */
+    public static Date yearLastDay(Date date){
+        if(date == null){
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int lastDay = c.getActualMaximum(Calendar.DAY_OF_YEAR);//获取某年最大天数
+        c.set(Calendar.DAY_OF_YEAR,lastDay);//设置日历中年份的最大天数
+        return c.getTime();
+    }
+    /**
+     * <p>@Description 获取日期所在年份的最后一天 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/4/14 22:19 </p>
+     * @param date 日期
+     * @param formatEnum 日期格式
+     * @return 日期所在年份的最后一天
+     */
+    public static String yearLastDay(Date date,DateFormatEnum formatEnum){
+        Date firstDate = yearLastDay(date);
+        return format(firstDate,formatEnum);
+    }
+    /**
+     * 在给定的日期加上或减去指定格式后的日期
+     * @param sourceDate 原始时间
+     * @param calendar   要调整的格式，Calendar类的属性
+     * @param num      要调整的数值，向前为负数，向后为正数
+     * @return
+     */
+    public static Date stepDate(Date sourceDate, int calendar,int num) {
+        if(sourceDate == null){
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(sourceDate);
+        c.add(calendar, num);
+        return c.getTime();
+    }
+    /**
      * 在给定的日期加上或减去指定月份后的日期
      * @param sourceDate 原始时间
      * @param month      要调整的月份，向前为负数，向后为正数
