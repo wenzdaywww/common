f79bd7020d9b3d88e37fffbb653e07ada9fc5c99
wenzdaywww <289209496@qq.com>
2023-06-07 21:40:33 +0800

    添加字符串转换工具类

--- src/main/java/com/www/common/config/exception/BusinessException.java
+++ src/main/java/com/www/common/config/exception/BusinessException.java
@@ -1,5 +1,6 @@
 package com.www.common.config.exception;
 
+import com.www.common.utils.MyStringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -32,12 +33,38 @@ public class BusinessException extends RuntimeException{
      * <p>@Description 业务异常 </p>
      * <p>@Author www </p>
      * <p>@Date 2023/3/22 22:48 </p>
+     * @param code 异常码
+     * @param msg 异常信息
+     * @param valArr 异常信息设置值
+     * @return
+     */
+    public BusinessException(int code,String msg,Object... valArr){
+        super(MyStringUtils.format(msg,valArr));
+        this.code = code;
+        this.msg = MyStringUtils.format(msg,valArr);
+    }
+    /**
+     * <p>@Description 业务异常 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/3/22 22:48 </p>
      * @param msg 异常信息
      * @return
      */
     public BusinessException(String msg){
         super(msg);
         this.msg = msg;
+    }
+    /**
+     * <p>@Description 业务异常 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/3/22 22:48 </p>
+     * @param msg 异常信息
+     * @param valArr 异常信息设置值
+     * @return
+     */
+    public BusinessException(String msg,Object... valArr){
+        super(MyStringUtils.format(msg,valArr));
+        this.msg = MyStringUtils.format(msg,valArr);
     }
 
     public int getCode() {
---
+++ src/main/java/com/www/common/utils/MyStringUtils.java
@@ -0,0 +1,35 @@
+package com.www.common.utils;
+
+import com.www.common.data.constant.CharConstant;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * <p>@Description 字符串工具类 </p>
+ * <p>@Version 1.0 </p>
+ * <p>@Author www </p>
+ * <p>@Date 2023/6/7 21:10 </p>
+ */
+public class MyStringUtils {
+    /**
+     * <p>@Description 字符串中的{}设置值 </p>
+     * <p>@Author www </p>
+     * <p>@Date 2023/6/7 21:25 </p>
+     * @param msg    待设置的字符串
+     * @param valArr 待设置的值
+     * @return 替换后的字符串
+     */
+    public static String format(String msg, Object... valArr) {
+        if (StringUtils.isBlank(msg) || valArr == null || valArr.length == 0) {
+            return msg;
+        }
+        StringBuilder sb = new StringBuilder(msg);
+        for (int i = 0; i < valArr.length; i++) {
+            int index = sb.indexOf(CharConstant.LEFT_BRACE + CharConstant.RIGHT_BRACE);
+            if (index > 0) {
+                String val = valArr[i] == null ? "" : valArr[i].toString();
+                sb.replace(index, index + 2, val);
+            }
+        }
+        return sb.toString();
+    }
+}
